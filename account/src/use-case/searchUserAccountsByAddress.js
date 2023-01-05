d83406184ccb081b5fc8b84fd5e798b988c48ba5
Gah2k0 <gah.francisco@gmail.com>
2023-01-05 15:37:05 -0300

    code refactoring, search user by address

--- account/src/use-case/searchUserAccountsByAddress.js
+++ account/src/use-case/searchUserAccountsByAddress.js
@@ -1,9 +1,7 @@
 import { userAccounts } from "./createUserAccount.js";
 
 export function searchUserAccountsByAddressUseCase(address){
-    const searchedUsersWithAddresses = userAccounts.filter(userAccount => userAccount.hasOwnProperty("address"));
-
-    const filteredSearchedUsers = searchedUsersWithAddresses.filter(userAccount => userAccount.address.uf === address);
+    const filteredSearchedUsers = userAccounts.filter(userAccount => userAccount.address?.uf === address);
 
     return filteredSearchedUsers;
 }
--- account/test/searchUserAccountsByAddress.test.js
+++ account/test/searchUserAccountsByAddress.test.js
@@ -7,6 +7,7 @@ createUserUseCase("Paula Francisco", "paula@email.com", "12345678");
 createUserUseCase("Jose Francisco", "jose@email.com", "12345678");
 createUserUseCase("Gandalf o Cinzento", "gandalf@email.com", "12345678");
 createUserUseCase("Frodo Bolseiro", "bolsao@email.com", "12345678");
+createUserUseCase("Aragorn", "strider@email.com", "12345678");
 
 addUserAddressUseCase("gabriel@email.com", "Rua Cascata", 87, "Casa", "Natal", "94015-380", "Gravataí", "RS");
 addUserAddressUseCase("paula@email.com", "Rua Cascata", 87, "Casa", "Natal", "94015-380", "Gravataí", "RS");
@@ -14,4 +15,4 @@ addUserAddressUseCase("jose@email.com", "Rua Bonita", 101, "Casa", "Qualquer Bai
 addUserAddressUseCase("gandalf@email.com", "Rua Lindinha", 101, "Casa", "Qualquer Bairro", "94015-380", "São Paulo", "SP");
 addUserAddressUseCase("bolsao@email.com", "Bolsão", 101, "Casa", "Vila dos Hobbits", "94015-380", "Condado", "SP");
 
-console.log(searchUserAccountsByAddressUseCase("MG"));
+console.log(searchUserAccountsByAddressUseCase("RS"));
