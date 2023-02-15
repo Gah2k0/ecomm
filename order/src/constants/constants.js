5efaa75970e02b48bc9d5a4157e8f9c7286a907e
Gah2k0 <gah.francisco@gmail.com>
2023-02-15 15:58:26 -0300

    fix: order validation to create and confirm

---
+++ order/src/constants/constants.js
@@ -0,0 +1,6 @@
+const ORDER_STATUS = {
+    REALIZADO: "REALIZADO",
+    PAGO: "PAGO"
+};
+
+export default ORDER_STATUS;
--- order/src/controllers/orderController.js
+++ order/src/controllers/orderController.js
@@ -1,5 +1,6 @@
 import Order from '../models/Order.js';
 import { fetchAccount, fetchConfirmPayment } from '../utils/fetchApi.js';
+import ORDER_STATUS from '../constants/constants.js';
 
 class OrderController {
 
@@ -18,23 +19,25 @@ class OrderController {
             };
         });
     }
-    static createOrder = (req, res) => {
-        let order = new Order({...req.body, status: "REALIZADO"});
-
-        order.save((error) => {
-            if(error){
-                res.status(500).send({message: `${error.message}`});
-            } else { 
+    static createOrder = async (req, res) => {
+        let order = new Order({...req.body, status: ORDER_STATUS.REALIZADO});
+        try{
+            const {name, cpf, address} = await fetchAccount(order.customerId);
+            if(!name || !cpf || !address)
+                return res.status(400).send({message: "The informed customer is invalid. Create an account to make an order."});
+            order.save((_) => {
                 res.status(201).send(order.toJSON());
-            };
-        });
+            });
+        } catch(error){
+            res.status(500).send({message: `${error.message}`});
+        }
     }
     static confirmOrder = async (req, res) => {
         const { id, paymentId } = req.params;
         try{
             const order = await Order.findById(id);
-            if(!order)
-                return res.status(404).send({message: "This order does not exist"});
+            if(!order || order.status !== ORDER_STATUS.REALIZADO)
+                return res.status(404).send({message: "This order does not exist or is already confirmed."});
             const {name, cpf, address} = await fetchAccount(order.customerId);
             if(!name || !cpf || !address)
                 return res.status(400).send({message: "This order does not have a valid account for confirming. Please contact the support"});
@@ -47,7 +50,7 @@ class OrderController {
             };
             const paymentConfirmation = await fetchConfirmPayment(paymentId, payLoad);
             if(paymentConfirmation){
-                await Order.findByIdAndUpdate(id, {status: "PAGO"});
+                await Order.findByIdAndUpdate(id, {status: ORDER_STATUS.PAGO});
                 return res.status(200).send(paymentConfirmation);
             }
             return res.status(400).send({message: "The order operation could not be completed because your payment could not be confirmed."});
