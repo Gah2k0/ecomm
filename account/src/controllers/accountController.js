a5554fc271c74aa1efbfd497bc2ff9952ae89a77
Gah2k0 <gah.francisco@gmail.com>
2023-02-24 17:41:04 -0300

    fix: eslint account API

--- account/src/controllers/accountController.js
+++ account/src/controllers/accountController.js
@@ -2,63 +2,57 @@ import Account from '../models/Account.js';
 import validateAccount from '../validations/accountValidation.js';
 
 class AccountController {
-    static getAllAccounts = (_req, res) => {
-        Account.find((errors, accounts) => {
-            if(!errors)
-                res.status(200).json(accounts);
-        })
-    };
-    static createAccount = (req, res) => {
-        const account = new Account(req.body);
-        try{
-            const accountValidationErrors = validateAccount(account);
-            if(accountValidationErrors.length > 0)
-                return res.status(400).send({message: accountValidationErrors});
-            account.save((_) => {
-                return res.status(201).json(account);
-            })
-        } catch(error) {
-            return res.status(500).send({message: `${error.message}`});
-        }
-    };
-    static getAccountById = (req, res) => {
-        const { id } = req.params;
-        try {
-            Account.findById(id, (errors, account) => {
-                if(account)
-                    return res.status(200).json(account);
-                else(!account)
-                    return res.status(404).send({message: 'User not found'});
-            })
-        } catch (error) {
-            return res.status(500).send({message: error.message});
-        }
-    };
-    static updatedAccount = (req, res) => {
-        const { id } = req.params;
-        const updatedAccount = req.body;
-        try {
-            const accountValidationErrors = validateAccount(updatedAccount);
-            if(accountValidationErrors.length > 0)
-                return res.status(400).send({message: accountValidationErrors});
-            
-            Account.findByIdAndUpdate(id, updatedAccount, (_) => {
-                    return res.status(200).send("Account succesfully updated!");
-            });
-        } catch (error) {
-            return res.status(500).send({message: `${error.message}`});
-        }
-    };
-    static deleteAccount = (req, res) => {
-        let { id } = req.params;
-        try {
-            Account.findByIdAndDelete(id, (error) => {
-                return res.status(200).send("Account succesfully deleted!");
-            });
-        } catch (error) {
-            return res.status(500).send({message: `${error.message}`});
-        }
-    };
-};
+  static getAllAccounts = (_req, res) => {
+    Account.find((errors, accounts) => {
+      if (!errors) { res.status(200).json(accounts); }
+    });
+  };
+
+  static createAccount = (req, res) => {
+    const account = new Account(req.body);
+    try {
+      const accountValidationErrors = validateAccount(account);
+      if (accountValidationErrors.length > 0) { return res.status(400).send({ message: accountValidationErrors }); }
+      account.save((_) => res.status(201).json(account));
+    } catch (error) {
+      return res.status(500).send({ message: `${error.message}` });
+    }
+  };
+
+  static getAccountById = (req, res) => {
+    const { id } = req.params;
+    try {
+      Account.findById(id, (errors, account) => {
+        if (account) { return res.status(200).json(account); }
+        (!account);
+        return res.status(404).send({ message: 'User not found' });
+      });
+    } catch (error) {
+      return res.status(500).send({ message: error.message });
+    }
+  };
+
+  static updatedAccount = (req, res) => {
+    const { id } = req.params;
+    const updatedAccount = req.body;
+    try {
+      const accountValidationErrors = validateAccount(updatedAccount);
+      if (accountValidationErrors.length > 0) { return res.status(400).send({ message: accountValidationErrors }); }
+
+      Account.findByIdAndUpdate(id, updatedAccount, (_) => res.status(200).send('Account succesfully updated!'));
+    } catch (error) {
+      return res.status(500).send({ message: `${error.message}` });
+    }
+  };
+
+  static deleteAccount = (req, res) => {
+    const { id } = req.params;
+    try {
+      Account.findByIdAndDelete(id, (error) => res.status(200).send('Account succesfully deleted!'));
+    } catch (error) {
+      return res.status(500).send({ message: `${error.message}` });
+    }
+  };
+}
 
 export default AccountController;
--- account/src/routes/index.js
+++ account/src/routes/index.js
@@ -1,21 +1,20 @@
 import express from 'express';
-import accounts from './accountRoutes.js';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
+import accounts from './accountRoutes.js';
 
 const swaggerDocument = YAML.load('./swagger/account.yaml');
 
 const routes = (app) => {
-    app.route('/').get((req, res) => {
-        res.status(200).send({titulo: "PROJETO ECOMM"})
-    })
+  app.route('/').get((req, res) => {
+    res.status(200).send({ titulo: 'PROJETO ECOMM' });
+  });
 
-    app.use(
-        express.json(),
-        accounts
-    )
-    app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  app.use(
+    express.json(),
+    accounts,
+  );
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+};
 
-}
-
-export default routes
+export default routes;
