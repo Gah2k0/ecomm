4f5e9203672be3cdce6ae477a36973f175e1f84b
Gah2k0 <gah.francisco@gmail.com>
2023-03-09 18:09:23 -0300

    fix: removing a console.log

--- account/src/auth/createToken.js
+++ account/src/auth/createToken.js
@@ -4,7 +4,6 @@ const createJwtToken = (account) => {
   const payload = {
     id: account._id,
   };
-  console.log(process.env.JWT_SECRET);
   const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '15m' });
   return token;
 };
