7e8d3bbd9f59b6a216c1e68dc1982a1becd0d22a
Gah2k0 <gah.francisco@gmail.com>
2023-03-10 14:19:32 -0300

    feature: finance API checking token blocklist

--- finance/api/auth/bearerStrategy.js
+++ finance/api/auth/bearerStrategy.js
@@ -1,11 +1,18 @@
 const BearerStrategy = require('passport-http-bearer').Strategy;
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
+const tokenExists = require('../../redis/blacklistFunctions.js');
+
+async function verifyTokenOnBlacklist(token) {
+  const isTokenOnBlacklist = await tokenExists(token);
+  if (isTokenOnBlacklist) throw new jwt.JsonWebTokenError('Logout has been made with this token');
+}
 
 passport.use(
   new BearerStrategy(
     async (token, done) => {
       try {
+        await verifyTokenOnBlacklist(token);
         const payload = jwt.verify(token, process.env.JWT_SECRET);
         // const account = await Account.findById(payload.id);
         done(null, payload.id);
--- finance/server.js
+++ finance/server.js
@@ -1,5 +1,6 @@
-const app = require('./api/app.js');
 const dotenv = require('dotenv').config();
+const app = require('./api/app.js');
+const blacklist = require('./redis/blacklist.js');
 
 const port = process.env.PORT || 3004;
 
