d3cf90b7b5090bd7ad122bfa16b74fdc01b843e1
Gah2k0 <gah.francisco@gmail.com>
2023-03-13 16:19:22 -0300

    fix: salt in hashPassword function

--- account/src/utils/passwordHashing.js
+++ account/src/utils/passwordHashing.js
@@ -1,5 +1,6 @@
 import bcryptjs from 'bcryptjs';
 
-const hashPassword = (password) => bcryptjs.hashSync(password);
+const salt = bcryptjs.genSaltSync(10);
+const hashPassword = (password) => bcryptjs.hashSync(password, salt);
 
 export default hashPassword;
