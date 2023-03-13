a3e2137477896564a51d78954495ab10fc99b98d
Gah2k0 <gah.francisco@gmail.com>
2023-03-13 16:22:23 -0300

    fix: comments removal

--- finance/api/auth/bearerStrategy.js
+++ finance/api/auth/bearerStrategy.js
@@ -14,7 +14,6 @@ passport.use(
       try {
         await verifyTokenOnBlacklist(token);
         const payload = jwt.verify(token, process.env.JWT_SECRET);
-        // const account = await Account.findById(payload.id);
         done(null, payload.id);
       } catch (error) {
         done(error);
--- order/src/auth/bearerStrategy.js
+++ order/src/auth/bearerStrategy.js
@@ -16,7 +16,6 @@ passport.use(
       try {
         await verifyTokenOnBlacklist(token);
         const payload = jwt.verify(token, process.env.JWT_SECRET);
-        // const account = await Account.findById(payload.id);
         done(null, payload.id);
       } catch (error) {
         done(error);
