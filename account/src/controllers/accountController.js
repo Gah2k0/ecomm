d2c89859ec1982a539960f0b22177e58f8e9a490
Gah2k0 <gah.francisco@gmail.com>
2023-03-14 11:03:28 -0300

    fix: mongoose operations with async/await

--- account/src/controllers/accountController.js
+++ account/src/controllers/accountController.js
@@ -5,54 +5,65 @@ import createJwtToken from '../auth/createToken.js';
 import { addToken } from '../../redis/blacklistFunctions.js';
 
 class AccountController {
-  static getAllAccounts = (_req, res) => {
-    Account.find((errors, accounts) => {
-      if (!errors) { res.status(200).json(accounts); }
-    });
+  static getAllAccounts = async (_req, res) => {
+    try {
+      const result = await Account.find();
+      return res.status(200).send(result);
+    } catch (error) {
+      return res.status(500).send({ message: error.message });
+    }
   };
 
   static createAccount = async (req, res) => {
     const account = new Account(req.body);
     try {
       const accountValidationErrors = validateAccount(account);
-      if (accountValidationErrors.length > 0) { return res.status(400).send({ message: accountValidationErrors }); }
+      if (accountValidationErrors.length > 0) {
+        return res.status(400).send({ message: accountValidationErrors });
+      }
 
       account.password = hashPassword(account.password);
-      return account.save(() => res.status(201).json(account));
+      const result = await account.save();
+      return res.status(201).json(result);
     } catch (error) {
       return res.status(500).send({ message: `${error.message}` });
     }
   };
 
-  static getAccountById = (req, res) => {
+  static getAccountById = async (req, res) => {
     const { id } = req.params;
     try {
-      return Account.findById(id, (errors, account) => {
-        if (account) { return res.status(200).json(account); }
-        return res.status(404).send({ message: 'User not found' });
-      });
+      const account = await Account.findById(id);
+      if (account) {
+        return res.status(200).json(account);
+      }
+      return res.status(404).send({ message: 'User not found' });
     } catch (error) {
       return res.status(500).send({ message: error.message });
     }
   };
 
-  static updateAccount = (req, res) => {
+  static updateAccount = async (req, res) => {
     const { id } = req.params;
     const updatedAccount = req.body;
     try {
       const accountValidationErrors = validateAccount(updatedAccount);
-      if (accountValidationErrors.length > 0) { return res.status(400).send({ message: accountValidationErrors }); }
+      if (accountValidationErrors.length > 0) {
+        return res.status(400).send({ message: accountValidationErrors });
+      }
 
-      return Account.findByIdAndUpdate(id, updatedAccount, () => res.status(200).send('Account succesfully updated!'));
+      await Account.findByIdAndUpdate(id, updatedAccount);
+      return res.status(200).send('Account succesfully updated!');
     } catch (error) {
       return res.status(500).send({ message: `${error.message}` });
     }
   };
 
-  static deleteAccount = (req, res) => {
+  static deleteAccount = async (req, res) => {
     const { id } = req.params;
     try {
-      return Account.findByIdAndDelete(id, () => res.status(204).send());
+      await Account.findByIdAndDelete(id);
+      return res.status(204).send();
     } catch (error) {
       return res.status(500).send({ message: `${error.message}` });
     }
