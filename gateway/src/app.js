10327f6a9818d72da27c06b65262b296a7747a04
Gah2k0 <gah.francisco@gmail.com>
2023-03-16 16:43:46 -0300

    feature: order API proxy

--- gateway/src/app.js
+++ gateway/src/app.js
@@ -3,6 +3,7 @@ import productRoutes from './routes/productService/productRoute.js';
 import categoryRoutes from './routes/productService/categoryRoute.js';
 import accountRoutes from './routes/accountService/accountRoutes.js';
 import financeRoutes from './routes/financeService/financeRoutes.js';
+import orderRoutes from './routes/orderService/orderRoutes.js';
 import './authentication/bearerStrategy.js';
 
 const app = express();
@@ -13,5 +14,6 @@ app.use('/', productRoutes);
 app.use('/', categoryRoutes);
 app.use('/', accountRoutes);
 app.use('/', financeRoutes);
+app.use('/', orderRoutes);
 
 export default app;
