5e1530238fca6b8cda81c0e73035da307e3f318c
Gah2k0 <gah.francisco@gmail.com>
2023-03-16 16:16:46 -0300

    feature: finance API proxy

--- gateway/src/app.js
+++ gateway/src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import productRoutes from './routes/productService/productRoute.js';
 import categoryRoutes from './routes/productService/categoryRoute.js';
 import accountRoutes from './routes/accountService/accountRoutes.js';
+import financeRoutes from './routes/financeService/financeRoutes.js';
 import './authentication/bearerStrategy.js';
 
 const app = express();
@@ -11,5 +12,6 @@ app.use(express.json());
 app.use('/', productRoutes);
 app.use('/', categoryRoutes);
 app.use('/', accountRoutes);
+app.use('/', financeRoutes);
 
 export default app;
---
+++ gateway/src/routes/financeService/financeRoutes.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import httpProxy from 'express-http-proxy';
+import authBearerMiddleware from '../../authentication/bearerMiddleware.js';
+
+const financeRoutes = express.Router();
+
+const host = process.env.FINANCE_HOST || 'localhost';
+const financeServiceUrl = `http://${host}:3004`;
+
+financeRoutes
+    .get('/payments/:id', authBearerMiddleware, httpProxy(financeServiceUrl, { timeout: 3000 }))
+    .post('/payments', authBearerMiddleware, httpProxy(financeServiceUrl, { timeout: 3000 }))
+    .post('/payments/:id/confirm', authBearerMiddleware, httpProxy(financeServiceUrl, { timeout: 3000 }))
+    .patch('/payments/:id/cancel', authBearerMiddleware, httpProxy(financeServiceUrl, { timeout: 3000 }));
+
+export default financeRoutes;
