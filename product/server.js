43d269ce12ce6755bb6c5db8c5cb6616fb076529
Gah2k0 <gah.francisco@gmail.com>
2023-03-17 09:18:46 -0300

    fix: removing authentication logic product API

--- product/server.js
+++ product/server.js
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv';
 import app from './src/app.js';
 import db from './src/config/dbConnect.js';
-import blacklist from './redis/blacklist.js';
 
 dotenv.config();
 
--- product/src/routes/productsRoute.js
+++ product/src/routes/productsRoute.js
@@ -1,14 +1,13 @@
 import express from 'express';
 import ProductController from '../controllers/productController.js';
-import authBearerMiddleware from '../auth/bearerMiddleware.js';
 
 const router = express.Router();
 
 router
   .get('/api/products', ProductController.getAllProducts)
   .get('/api/products/:id', ProductController.getProductById)
-  .post('/api/admin/products', authBearerMiddleware, ProductController.createProduct)
-  .put('/api/admin/products/:id', authBearerMiddleware, ProductController.updateProduct)
-  .delete('/api/admin/products/:id', authBearerMiddleware, ProductController.deleteProduct);
+  .post('/api/admin/products', ProductController.createProduct)
+  .put('/api/admin/products/:id', ProductController.updateProduct)
+  .delete('/api/admin/products/:id', ProductController.deleteProduct);
 
 export default router;
